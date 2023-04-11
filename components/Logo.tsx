4efe1c95ef33974f2ef1e9c4b97e717e7bc07186
Ruslan Mochulskyy <mochulskyy.ruslan@gmail.com>
2023-04-11 22:49:13 +0200

    Initial tailwind setup

--- components/Logo.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Logo: React.FC = () => {
-    return <div className="logo">Ruslan Mochulskyy</div>;
-};
-
-export default Logo;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,6 @@
 import '../styles/globals.scss'
 import type {AppProps} from 'next/app'
-import Navbar from "../components/Navbar";
 
 export default function App({Component, pageProps}: AppProps) {
-    return (
-        <>
-            <Navbar/>
-            <Component {...pageProps} />
-        </>
-    )
+    return <Component {...pageProps} />
 }
