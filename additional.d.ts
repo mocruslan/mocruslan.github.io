9d16dc2075f35b5879f3b82914f0ca8958c5b2d9
Ruslan Mochulskyy <mochulskyy.ruslan@gmail.com>
2023-04-25 21:13:49 +0200

    Added environment variable usage

---
+++ additional.d.ts
@@ -0,0 +1,10 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            CONTACT_FORM_ENDPOINT: string;
+            CONTACT_FORM_API_KEY: string;
+        }
+    }
+}
+
+export {};
